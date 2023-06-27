d2da406230188ec6da823201463d4450f83ebd35
Meobot <nathanrhodes312@gmail.com>
2023-06-26 22:09:59 -0400

    hardcoded form for testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore";
 import { namesCollection } from "./firebase";
 
+// {names[0]?.humanNames.map((name, index) => (
+// 	<li key={index}>{name}</li>
+// ))}
+
 function App() {
 	const [names, setNames] = useState<string[]>([]);
 
@@ -20,14 +24,93 @@ function App() {
 	};
 
 	return (
-		<>
-			<h1>Names</h1>
-			<ul>
-				{names[0]?.humanNames.map((name, index) => (
-					<li key={index}>{name}</li>
-				))}
-			</ul>
-		</>
+		<div className="container p-6 sm:grid sm:grid-cols-3 sm:gap-5">
+			<div className="bg-slate-500 flex flex-col justify-center items-center py-4 sm:col-span-1">
+				<h1>NPC Dispenser</h1>
+				<div className="flex flex-col items-start w-3/4 space-y-2">
+					<label htmlFor="species">Species</label>
+					<select name="species" id="species" className="w-full">
+						<option value="human">Human</option>
+						<option value="elf">Elf</option>
+						<option value="dwarf">Dwarf</option>
+						<option value="halfling">Halfling</option>
+						<option value="gnome">Gnome</option>
+						<option value="dragonborn">Dragonborn</option>
+						<option value="tiefling">Tiefling</option>
+						<option value="half-elf">Half-Elf</option>
+						<option value="half-orc">Half-Orc</option>
+						<option value="aasimar">Aasimar</option>
+						<option value="firbolg">Firbolg</option>
+						<option value="goliath">Goliath</option>
+					</select>
+					<label htmlFor="gender">Gender</label>
+					<select name="gender" id="gender" className="w-full">
+						<option value="male">Male</option>
+						<option value="female">Female</option>
+						<option value="non-binary">Non-binary</option>
+						<option value="other">Other</option>
+					</select>
+					<label htmlFor="age">Age</label>
+					<select name="age" id="age" className="w-full">
+						<option value="child">Child</option>
+						<option value="young adult">Young Adult</option>
+						<option value="adult">Adult</option>
+						<option value="elder">Elder</option>
+					</select>
+					<label htmlFor="occupation">Occupation</label>
+					<select
+						name="occupation"
+						id="occupation"
+						className="w-full"
+					>
+						<option value="adventurer">Adventurer</option>
+						<option value="artisan">Artisan</option>
+						<option value="criminal">Criminal</option>
+						<option value="entertainer">Entertainer</option>
+						<option value="farmer">Farmer</option>
+						<option value="fisherman">Fisherman</option>
+						<option value="herder">Herder</option>
+						<option value="hunter">Hunter</option>
+						<option value="laborer">Laborer</option>
+						<option value="merchant">Merchant</option>
+					</select>
+					<label htmlFor="personality">Personality</label>
+					<select
+						name="personality"
+						id="personality"
+						className="w-full"
+					>
+						<option value="arrogant">Arrogant</option>
+						<option value="belligerent">Belligerent</option>
+						<option value="boastful">Boastful</option>
+						<option value="bold">Bold</option>
+						<option value="brave">Brave</option>
+						<option value="cautious">Cautious</option>
+						<option value="charming">Charming</option>
+						<option value="cheerful">Cheerful</option>
+						<option value="cowardly">Cowardly</option>
+					</select>
+					<label htmlFor="quirk">Quirk</label>
+					<select name="quirk" id="quirk" className="w-full">
+						<option value="always hungry">Always hungry</option>
+						<option value="always tired">Always tired</option>
+						<option value="always thirsty">Always thirsty</option>
+						<option value="always wants to fight">
+							Always wants to fight
+						</option>
+						<option value="always wants to talk">
+							Always wants to talk
+						</option>
+					</select>
+					<div className="w-full flex justify-center">
+						<button className="bg-blue-500 px-6 py-2 rounded-xl">
+							Generate
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className="sm:col-span-2"></div>
+		</div>
 	);
 }
 
