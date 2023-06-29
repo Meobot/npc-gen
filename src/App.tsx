27f5999afcbfcc4ef1140ad520cc74d40e245ad4
Meobot <nathanrhodes312@gmail.com>
2023-06-28 21:27:11 -0400

    data uploaded to database / removed function

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import { useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore";
-import { namesCollection } from "./firebase";
+import { npcDataCollection } from "./firebase";
 
-// {names[0]?.humanNames.map((name, index) => (
+// {npcData[0]?.humanNames.map((name, index) => (
 // 	<li key={index}>{name}</li>
 // ))}
 
 function App() {
-	const [names, setNames] = useState<string[]>([]);
-
-	useEffect(() => {
-		fetchNamesFromFirestore();
-	}, []);
-
-	const fetchNamesFromFirestore = async () => {
-		try {
-			const querySnapshot = await getDocs(namesCollection);
-			const namesList = querySnapshot.docs.map((doc) => doc.data()); // Access the 'name' field specifically
-			setNames(namesList);
-		} catch (error) {
-			console.error("Error fetching names from Firestore:", error);
-		}
-	};
 
 	return (
 		<div className="container p-6 sm:grid sm:grid-cols-3 sm:gap-5">
