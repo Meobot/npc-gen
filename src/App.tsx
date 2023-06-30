53596f3ad5bcd91c75d0dbdba0d3da4422c3f09d
Meobot <nathanrhodes312@gmail.com>
2023-06-30 17:00:02 -0400

    moved effects to App for testing / rework needed

--- src/App.tsx
+++ src/App.tsx
@@ -5,90 +5,122 @@ import Sex from "./components/Sex";
 import Alignment from "./components/Alignment";
 import MainDetails from "./components/MainDetails";
 import namesList from "./namesList"
-
-// {npcData[0]?.humanNames.map((name, index) => (
-// 	<li key={index}>{name}</li>
-// ))}
+import { getDataFromField } from "./firebase";
 
 function App() {
-	const [species, setSpecies] = useState("");
-	const [sex, setSex] = useState("");
-	const [alignment, setAlignment] = useState("");
+    const [species, setSpecies] = useState("");
+    const [sex, setSex] = useState("");
+    const [alignment, setAlignment] = useState("");
 
-	//function to pull a random name from the names array
-	const getRandomName = (namesArray) => {
-		let randomIndex = Math.floor(Math.random() * namesArray.length);
-		let randomName = namesArray[randomIndex];
-		return randomName;
-	};
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
 
-	useEffect(() => {
-		setSpecies(getRandomName(namesList));
-	}, []);
+    //function to pull a random name from the names array
+    const getRandomName = (namesArray) => {
+        let randomIndex = Math.floor(Math.random() * namesArray.length);
+        let randomName = namesArray[randomIndex];
+        return randomName;
+    };
 
-	const handleSpeciesChange = (
-		event: React.ChangeEvent<HTMLSelectElement>
-	) => {
-		setSpecies(event.target.value);
-	};
+    useEffect(() => {
+        setSpecies(getRandomName(namesList));
+    }, []);
 
-	const handleSexChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		setSex(event.target.value);
-	};
+    useEffect(() => {
+        const firstNamesField = sex === "male" ? "maleNames" : "femaleNames";
+        getDataFromField("namesDoc", firstNamesField).then((data) => {
+            const randomName = getRandomName(data);
+            if (randomName === undefined) {
+                setFirstName("random");
+            } else {
+                setFirstName(randomName);
+            }
+        });
+    }, [sex]);
 
-	const handleAlignmentChange = (
-		event: React.ChangeEvent<HTMLSelectElement>
-	) => {
-		setAlignment(event.target.value);
-	};
+    useEffect(() => {
+        if (species) {
+            const lastNamesField = `${species}Names`;
+            getDataFromField("namesDoc", lastNamesField).then((data) => {
+                const randomName = getRandomName(data);
+                if (randomName === undefined) {
+                    setLastName("random");
+                } else {
+                    setLastName(randomName);
+                }
+            });
+        }
+    }, [species]);
 
-	return (
-		<div className="container p-6 sm:grid sm:grid-cols-3 sm:gap-5">
-			<div className="bg-slate-500 flex flex-col justify-center items-center py-4 mb-8 sm:col-span-1">
-				<h1>NPC Dispenser</h1>
-				<div className="flex flex-col items-start w-3/4 space-y-2">
-					<Species handleSpeciesChange={handleSpeciesChange} />
-					<Sex handleSexChange={handleSexChange} />
-					<Alignment handleAlignmentChange={handleAlignmentChange} />
-					<div className="w-full flex justify-center">
-						<button className="bg-blue-500 px-6 py-2 rounded-xl">
-							Generate
-						</button>
-					</div>
-				</div>
-			</div>
-			<div className="bg-slate-800 text-white space-y-8 sm:col-span-2">
+    const handleSpeciesChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setSpecies(event.target.value);
+    };
 
-				<MainDetails 
-					species={species}
-					sex={sex}
-					alignment={alignment}
-				/>
+    const handleSexChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSex(event.target.value);
+    };
 
-				<div className="bg-orange-700">
-					<h2>Personality</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					</p>
-				M</div>
-				<div className="bg-green-900">
-					<h2>Ability Scores</h2>
-					<p>Strength: 10</p>
-					<p>Dexterity: 10</p>
-					<p>Constitution: 10</p>
-					<p>Intelligence: 10</p>
-					<p>Wisdom: 10</p>
-					<p>Charisma: 10</p>
-				</div>
-				<div className="bg-blue-900">
-					<h2>Relationships</h2>
-					<p>Sexual Orientation: Straight</p>
-					<p>Relationship Status: Single</p>
-					<p>Children: 0</p>
-				</div>
-			</div>
-		</div>
-	);
+    const handleAlignmentChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setAlignment(event.target.value);
+    };
+
+    const handleClick = () => {
+        setSpecies(getRandomName(namesList));
+    };
+
+    return (
+        <div className="container p-6 sm:grid sm:grid-cols-3 sm:gap-5">
+            <div className="bg-slate-500 flex flex-col justify-center items-center py-4 mb-8 sm:col-span-1">
+                <h1>NPC Dispenser</h1>
+                <div className="flex flex-col items-start w-3/4 space-y-2">
+                    <Species handleSpeciesChange={handleSpeciesChange} />
+                    <Sex handleSexChange={handleSexChange} />
+                    <Alignment handleAlignmentChange={handleAlignmentChange} />
+                    <div className="w-full flex justify-center">
+                        <button className="bg-blue-500 px-6 py-2 rounded-xl" onClick={handleClick}>
+                            Generate
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-slate-800 text-white space-y-8 sm:col-span-2">
+
+                <MainDetails 
+                    species={species}
+                    sex={sex}
+                    alignment={alignment}
+                    firstName={firstName}
+                    lastName={lastName}
+                />
+
+                <div className="bg-orange-700">
+                    <h2>Personality</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                M</div>
+                <div className="bg-green-900">
+                    <h2>Ability Scores</h2>
+                    <p>Strength: 10</p>
+                    <p>Dexterity: 10</p>
+                    <p>Constitution: 10</p>
+                    <p>Intelligence: 10</p>
+                    <p>Wisdom: 10</p>
+                    <p>Charisma: 10</p>
+                </div>
+                <div className="bg-blue-900">
+                    <h2>Relationships</h2>
+                    <p>Sexual Orientation: Straight</p>
+                    <p>Relationship Status: Single</p>
+                    <p>Children: 0</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/MainDetails.tsx
+++ src/components/MainDetails.tsx
@@ -1,42 +1,11 @@
 import { useEffect, useState } from "react";
-import { getDataFromField } from "../firebase";
-
 
 function MainDetails(props) {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
 
-    useEffect(() => {
-        const firstNamesField = props.sex === "male" ? "maleNames" : "femaleNames";
-        getDataFromField("namesDoc", firstNamesField).then((data) => {
-            let randomIndex = Math.floor(Math.random() * data.length);
-            let randomName = data[randomIndex];
-            if (randomName === undefined) {
-                setFirstName("random");
-            } else {
-                setFirstName(randomName);
-            }
-        });
-    }, [props.sex]);
-
-    useEffect(() => {
-        if (props.species) {
-            const lastNamesField = `${props.species}Names`;
-            getDataFromField("namesDoc", lastNamesField).then((data) => {
-                let randomIndex = Math.floor(Math.random() * data.length);
-                let randomName = data[randomIndex];
-                if (randomName === undefined) {
-                    setLastName("");
-                } else {
-                    setLastName(randomName);
-                }
-            });
-        }
-    }, [props.species]);
 
     return (
         <div>
-            <h2>{firstName} {lastName}</h2>
+            <h2>{props.firstName} {props.lastName}</h2>
         </div>
     );
 }
