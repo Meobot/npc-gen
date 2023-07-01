162da1cecdbbce9ad5949741c86872b316df7f9b
Meobot <nathanrhodes312@gmail.com>
2023-07-01 11:28:48 -0400

    reduced species options

--- src/components/Species.tsx
+++ src/components/Species.tsx
@@ -16,8 +16,6 @@ function Species({ handleSpeciesChange }) {
 				<option value="gnome">Gnome</option>
 				<option value="dragonborn">Dragonborn</option>
 				<option value="tiefling">Tiefling</option>
-				<option value="half-elf">Half-Elf</option>
-				<option value="half-orc">Half-Orc</option>
 			</select>
 		</>
 	);
