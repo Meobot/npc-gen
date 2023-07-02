eaf3654a23e8a014546408128ca7e56aef7a386a
Meobot <nathanrhodes312@gmail.com>
2023-07-01 21:09:24 -0400

    added name fetches back

--- src/components/MainDetails.tsx
+++ src/components/MainDetails.tsx
@@ -1,13 +1,36 @@
 import { useEffect, useState } from "react";
+import { getDataFromField } from "../firebase";
 
 function MainDetails(props) {
+	const getFirstName = () => {
+		const firstNamesField =
+			props.sex === "male" ? "maleNames" : "femaleNames";
+		getDataFromField("namesDoc", firstNamesField).then((data) => {
+			const randomName = props.getRandomValue(data);
+			props.setFirstName(randomName || "random");
+		});
+	};
 
+	const getLastName = () => {
+		const lastNamesField = `${props.species}Names`;
+		getDataFromField("namesDoc", lastNamesField).then((data) => {
+			const randomName = props.getRandomValue(data);
+			props.setLastName(randomName || "random");
+		});
+	};
 
-    return (
-        <div>
-            <h2>{props.firstName} {props.lastName}</h2>
-        </div>
-    );
+	useEffect(() => {
+		getFirstName();
+		getLastName();
+	}, []);
+	
+	return (
+		<div>
+			<h2>
+				{props.firstName} {props.lastName}
+			</h2>
+		</div>
+	);
 }
 
 export default MainDetails;
