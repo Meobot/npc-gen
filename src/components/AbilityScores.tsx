6beac2df72f58eff20ffe0bf660a75d83d4db97b
Meobot <nathanrhodes312@gmail.com>
2023-07-03 12:22:58 -0400

    scores memoized to prevent rerendering

--- src/components/AbilityScores.tsx
+++ src/components/AbilityScores.tsx
@@ -1,36 +1,39 @@
-function AbilityScores() {
-	const getAbilityScoreValues = () => {
-		const abilityScoreValues = {
-			strength: 0,
-			dexterity: 0,
-			constitution: 0,
-			intelligence: 0,
-			wisdom: 0,
-			charisma: 0,
-		};
+import React, { useMemo } from "react";
 
-		const abilityScoreKeys = Object.keys(abilityScoreValues);
+const AbilityScores = ({ firstName, lastName }) => {
+  const getAbilityScoreValues = useMemo(() => {
+    const abilityScoreValues = {
+      strength: 0,
+      dexterity: 0,
+      constitution: 0,
+      intelligence: 0,
+      wisdom: 0,
+      charisma: 0,
+    };
 
-		abilityScoreKeys.forEach((abilityScoreKey) => {
-			const randomValue = Math.floor(Math.random() * 15) + 3;
-			abilityScoreValues[
-				abilityScoreKey as keyof typeof abilityScoreValues
-			] = randomValue;
-		});
+    const abilityScoreKeys = Object.keys(abilityScoreValues);
 
-		return abilityScoreValues;
-	};
-	return (
-		<div className="px-3 py-5 border-2 border-orange-400 rounded-xl">
-			<h2 className="text-2xl mb-2">Ability Scores</h2>
-			<p>Strength: {getAbilityScoreValues().strength}</p>
-			<p>Dexterity: {getAbilityScoreValues().dexterity}</p>
-			<p>Constitution: {getAbilityScoreValues().constitution}</p>
-			<p>Intelligence: {getAbilityScoreValues().intelligence}</p>
-			<p>Wisdom: {getAbilityScoreValues().wisdom}</p>
-			<p>Charisma: {getAbilityScoreValues().charisma}</p>
-		</div>
-	);
-}
+    abilityScoreKeys.forEach((abilityScoreKey) => {
+      const randomValue = Math.floor(Math.random() * 15) + 3;
+      abilityScoreValues[
+        abilityScoreKey as keyof typeof abilityScoreValues
+      ] = randomValue;
+    });
+
+    return abilityScoreValues;
+  }, [firstName, lastName]);
+
+  return (
+    <div className="px-3 py-5 border-2 border-orange-400 rounded-xl">
+      <h2 className="text-2xl mb-2">Ability Scores</h2>
+      <p>Strength: {getAbilityScoreValues.strength}</p>
+      <p>Dexterity: {getAbilityScoreValues.dexterity}</p>
+      <p>Constitution: {getAbilityScoreValues.constitution}</p>
+      <p>Intelligence: {getAbilityScoreValues.intelligence}</p>
+      <p>Wisdom: {getAbilityScoreValues.wisdom}</p>
+      <p>Charisma: {getAbilityScoreValues.charisma}</p>
+    </div>
+  );
+};
 
 export default AbilityScores;
